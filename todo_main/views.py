6578ddaeed737c5122428de9954919524aa4611d
matg41 <matg131@yahoo.com>
2023-11-19 13:03:49 +0330

    add functionality to html and backend

--- todo_main/views.py
+++ todo_main/views.py
@@ -1,5 +1,9 @@
 #from django.http import HttpResponse
 from django.shortcuts import render
-
+from todo.models import Task
 def home(request):
-    return render(request, 'home.html')
+    tasks = Task.objects.filter(is_completed=False).order_by('modified_at')
+    context = {
+        "tasks": tasks
+    }
+    return render(request, 'home.html', context)
