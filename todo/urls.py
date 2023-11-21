d8796c97b72a4f19d5a70afa8ef13c12979892e2
matg41 <matg131@yahoo.com>
2023-11-21 08:21:49 +0330

    Adding functionality for editing and deleting tasks

--- todo/urls.py
+++ todo/urls.py
@@ -3,7 +3,16 @@ from . import views
 
 
 urlpatterns = [
+    #Adding Tasks
     path('addTask/', views.addTask, name='addTask'),
+    
+    #Mark as Done and Undone
     path('mark_as_done/<int:pk>', views.mark_as_done, name='mark_as_done'),
-    path('mark_as_undone/<int:pk>', views.mark_as_undone, name='mark_as_undone')
+    path('mark_as_undone/<int:pk>', views.mark_as_undone, name='mark_as_undone'),
+
+    #Edit tasks
+    path("edit_task/<int:pk>", views.edit_task, name='edit_task'),
+
+    #Delete task
+    path('delete_task/<int:pk>', views.delete_task, name='delete_task')
 ]
