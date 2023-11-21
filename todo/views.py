2b503be7b9d99c485713a83ab271943a894a1543
matg41 <matg131@yahoo.com>
2023-11-21 07:30:26 +0330

    Added Done and Undone functionality for every task

--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
 from django.http import HttpResponse
 from .models import Task
 
@@ -7,4 +7,16 @@ def addTask(request):
     task = request.POST['task']
     Task.objects.create(task=task)
 
+    return redirect('home')
+
+def mark_as_done(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.is_completed = True
+    task.save()
+    return redirect('home')
+
+def mark_as_undone(request,pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.is_completed = False
+    task.save()
     return redirect('home')
--- todo_main/views.py
+++ todo_main/views.py
@@ -3,7 +3,11 @@ from django.shortcuts import render
 from todo.models import Task
 def home(request):
     tasks = Task.objects.filter(is_completed=False).order_by('modified_at')
+
+    compeleted_tasks = Task.objects.filter(is_completed=True)
+    
     context = {
-        "tasks": tasks
+        "tasks": tasks,
+        'compeleted_tasks': compeleted_tasks,
     }
     return render(request, 'home.html', context)
